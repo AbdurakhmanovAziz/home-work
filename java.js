2e0306974523a5d2c985646efe96c29f0b2d0fca
Абдурахманов Зафаржон <110538209+AbdurakhmanovAziz@users.noreply.github.com>
2022-09-22 16:18:14 +0500

    home work
    home work

---
+++ java.js
@@ -0,0 +1,34 @@
+let name = 'Alex'
+let имя = prompt('кто ты воин ?')
+if(имя === name) {
+    alert('входи сын пелея')
+}   else{ 
+    alert('ты не сын пелея')
+}
+//
+let account = '7777'
+let number = prompt('код карты ')
+if(number === account) {
+   alert('баланс 10000 ')
+}   else{
+    alert('уходи бомж')
+}
+//
+let money = '10000'
+let nefor = prompt('сколько хочешь ?')
+if( nefor === money ) {
+} else if(
+    alert(`
+    Баланс был:${money}    
+    Остаток:${money - nefor}
+    `)
+){
+    alert('найс')
+} else (
+    alert('ты не настолько богат')
+)
+
+
+   
+     
+
